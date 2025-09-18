c1e8be013ad2f3c6aefc1f86e3c83441dc198f45
Jen Bird <jennifer.bird@awaze.com>
2025-09-18 14:59:07 +0100

    test base path

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
+ const repo = 'test-github-actions'; // <-- exact repo name
+
+const isProd = process.env.NODE_ENV === 'production';
+
+
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  assetPrefix: './',
+  output: 'export',                 // generate ./out
+  images: { unoptimized: true },    // needed for static export
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
+  trailingSlash: true,              // optional but helps with static hosting
 };
 
 export default nextConfig;
