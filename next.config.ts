769f6eb6b57baf789b515ceb64baa5904f51c460
Jen Bird <jennifer.bird@awaze.com>
2025-09-18 14:41:04 +0100

    Added assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   assetPrefix: './',
-  images: {
-    unoptimized: true,
-  },
-  
 };
 
 export default nextConfig;
